af8e854fa1ed8614a169810fa6228cae432d4026
root <root@AlecParkes.zones.eait.uq.edu.au>
2020-09-20 04:34:27 +0000

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<!-- Default copy paste html -->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="Title">
+  <meta name="author" content="test">
+
+  <link rel="stylesheet" href="css/!!!!!!">
+</head>
+
+<body>
+  <p>if you see this it worked</p>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="js/leaflet.js"></script>
+</body>
+</html>
