06c9128b51931debda01acff70af9c888021e244
AlanBark <a.parkes@uqconnect.edu.au>
2020-09-25 18:01:04 +0000

    added basic page structure to every php file for others to work off of

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,21 @@
 <?php
 	switch ($_SERVER["SCRIPT_NAME"]) {
-		case "/php-template/about.php":
+		case "/about.php":
 			$CURRENT_PAGE = "About"; 
 			$PAGE_TITLE = "About Us";
 			break;
-		case "/php-template/contact.php":
+		case "/contact.php":
 			$CURRENT_PAGE = "Contact"; 
 			$PAGE_TITLE = "Contact Us";
-			break;
+            break;
+        case "/map.php":
+            $CURRENT_PAGE = "Map"; 
+            $PAGE_TITLE = "Here is a map";
+            break;
+        case "/planning.php":
+            $CURRENT_PAGE = "Planning"; 
+            $PAGE_TITLE = "Planning page";
+            break;
 		default:
 			$CURRENT_PAGE = "Index";
 			$PAGE_TITLE = "Welcome to my homepage!";
--- includes/navigation.php
+++ includes/navigation.php
@@ -3,6 +3,12 @@
 	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
+      <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Map") {?>active<?php }?>" href="map.php">Map</a>
+	  </li>
+      <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Planning") {?>active<?php }?>" href="planning.php">Planning</a>
+	  </li>
 	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us</a>
 	  </li>
