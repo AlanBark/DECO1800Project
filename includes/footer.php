b17ab147b6cd095b61cea7435103d310b2f9b14d
Yee <y.p.goh@uqconnect.edu.au>
2020-09-28 13:21:29 +0000

    motified home page and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,26 @@
 <!--Footer include in every file. Currently only shows date with copywrite symbol-->
 <div class="footer">
-	&copy; <?php echo date("d M Y");?>
+
+
+	<div class="footer-container">
+		<div class="footer-item">
+		  	<h3>Save The Date</h3>
+		  	&copy; <?php echo date("d M Y");?>
+		</div>
+
+		<div class="footer-item">
+			<h4>Footer Links:</h4>
+
+		</div>
+
+	  	<div class="footer-item">
+	  		<h4>Stay In Touch:</h4>
+		  		<a href="#">Instagram</a>
+		  		<a href="#">Facebook</a>
+		</div>
+	</div>
+
+
+
+
 </div>
