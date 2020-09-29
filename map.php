7fcf5ab911390c2bc74fd446b4b6149163e209c1
Alec Parkes <a.parkes@uqconnect.edu.au>
2020-09-29 15:33:07 +0000

    Stuff i forgot

--- map.php
+++ map.php
@@ -2,13 +2,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- @TODO add this to header with conditional includes -->
     <script src='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
     <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
     <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css" type="text/css"/>
+    <link rel="stylesheet" href="css/map-style.css" type="text/css"/>
     <!-- Promise polyfill script required to use Mapbox GL Geocoder in IE 11 -->
     <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+    <script type="text/javascript" src="js/map.js"></script>
 	<?php include("includes/header.php");?>
     <?php include("includes/api-interaction.php");?>
 </head>
@@ -17,49 +20,29 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>Interactive Map</h2>
-    <div id='map' style='width: 800px; height: 800px;'></div>
-    <script>
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhbmJhcmsiLCJhIjoiY2tmbmtwamM3MDNqbzJ4cXRmZ2R4aGVxOSJ9.J_cyZxD5QAw8wyOQq-ompA';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/alanbark/ckfmoop0t69xa19qinltagru5', // stylesheet location
-        center: [146.5, -23.4], // starting position [lng, lat]
-        zoom: 4.6 // starting zoom
-    });
+    <h2>Interactive Map</h2>
 
-    // Adapted from https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-limit-region/
-    map.addControl(
-        new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            
-            // limit results to Australia
-            countries: 'au',
-            
-            // further limit results to the geographic bounds representing the region of
-            // Queensland
-            bbox: [137.95, -29.19, 154.44, -9.11],
-            
-            // apply a client side filter to further limit results to those strictly within
-            // the Queensland region
-            filter: function (item) {
-                // returns true if item contains Queensland region
-                return item.context
-                    .map(function (i) {
-                        // id is in the form {index}.{id} per https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
-                        // this example attempts to find the `region` named `New South Wales`
-                    return (
-                        i.id.split('.').shift() === 'region' &&
-                        i.text === 'Queensland'
-                    );
-                    })
-                .reduce(function (acc, cur) {
-                    return acc || cur;
+    <div id='map'>
+        <h4>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h4>
+        <button id="btnMap" type="button" class="btn btn-light">Lets get started</button>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#btnMap').click(function() {
+                $.fn.mapHandler();
+            });
+            $.fn.mapHandler = function() {
+                $.ajax({
+                    url: "includes/map-init.php",
+                    type: 'post',
+                    data: {"callinitialiseMap" : "1"},
+                    success: function(response) {
+                        var tmp=new Function (response);
+                        tmp();
+                    }
                 });
-            },
-            mapboxgl: mapboxgl
-        })
-    );
+            }
+        });
     </script>
 </div>
 <?php include("includes/footer.php");?>
