f8fbf3f0e51bcd0b543310213d6b8c8db238607c
Alec Parkes <a.parkes@uqconnect.edu.au>
2020-09-29 15:34:41 +0000

    added files forgot to before

---
+++ includes/map-init.php
@@ -0,0 +1,37 @@
+<?php
+include("api-interaction.php");
+
+function initialiseMap($data) {
+
+    // associative array of suburbs and marriage frequency
+    $suburbs = getAllSuburbs();
+
+    $allSuburbs = json_decode(file_get_contents('../js/raw-data.json'), true);
+    
+    $count = 0;
+    foreach($allSuburbs['features'] as $i) {
+        $found = false;
+        foreach($suburbs as $suburb => $frequency) {
+            if ($i['properties']['qld_loca_2'] == $suburb) {
+                $found = true;
+                $allSuburbs['features'][$count]['properties']['qld_loca_3'] = $frequency;
+                break;
+            }
+        }
+        if (!$found) {
+            unset($allSuburbs['features'][$count]);
+        }
+        $count++;
+    }
+    $newJson = json_encode($allSuburbs);
+    file_put_contents('../tmp/filtered.json', $newJson);
+
+    // @TODO call this file with parameters from map filter.
+    // initialise map
+    echo 'createMap();';
+}
+
+if (isset($_POST['callinitialiseMap'])) {
+    echo initialiseMap($_POST['callinitialiseMap']);
+}
+?>
