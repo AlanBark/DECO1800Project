66886b43adeffcb5121d50378f0532d6bc05f7b3
AlanBark <a.parkes@uqconnect.edu.au>
2020-09-29 07:38:34 +0000

    added search bar, limited to only qld regions

--- map.php
+++ map.php
@@ -2,6 +2,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <script src='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+    <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css" type="text/css"/>
+    <!-- Promise polyfill script required to use Mapbox GL Geocoder in IE 11 -->
+    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
 	<?php include("includes/header.php");?>
     <?php include("includes/api-interaction.php");?>
 </head>
@@ -11,17 +18,48 @@
 
 <div class="container" id="main-content">
 	<h2>Interactive Map</h2>
-    <div id="mapid"></div>
+    <div id='map' style='width: 800px; height: 800px;'></div>
     <script>
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoiYWxhbmJhcmsiLCJhIjoiY2tmbW5wcHU2MmQ2eDJxbG1mdmVlY3FleiJ9.N9kjUP7cI-ng_Yd7gSU0vA'
-        }).addTo(mymap);
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhbmJhcmsiLCJhIjoiY2tmbmtwamM3MDNqbzJ4cXRmZ2R4aGVxOSJ9.J_cyZxD5QAw8wyOQq-ompA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/alanbark/ckfmoop0t69xa19qinltagru5', // stylesheet location
+        center: [146.5, -23.4], // starting position [lng, lat]
+        zoom: 4.6 // starting zoom
+    });
+
+    // Adapted from https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-limit-region/
+    map.addControl(
+        new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            
+            // limit results to Australia
+            countries: 'au',
+            
+            // further limit results to the geographic bounds representing the region of
+            // Queensland
+            bbox: [137.95, -29.19, 154.44, -9.11],
+            
+            // apply a client side filter to further limit results to those strictly within
+            // the Queensland region
+            filter: function (item) {
+                // returns true if item contains Queensland region
+                return item.context
+                    .map(function (i) {
+                        // id is in the form {index}.{id} per https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+                        // this example attempts to find the `region` named `New South Wales`
+                    return (
+                        i.id.split('.').shift() === 'region' &&
+                        i.text === 'Queensland'
+                    );
+                    })
+                .reduce(function (acc, cur) {
+                    return acc || cur;
+                });
+            },
+            mapboxgl: mapboxgl
+        })
+    );
     </script>
 </div>
 <?php include("includes/footer.php");?>
