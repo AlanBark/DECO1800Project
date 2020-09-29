81aa587e671792e407abd22bd4afcc487b7d0057
AlanBark <a.parkes@uqconnect.edu.au>
2020-09-29 16:17:39 +0000

    added loading gif

--- map.php
+++ map.php
@@ -23,6 +23,7 @@
     <h2>Interactive Map</h2>
 
     <div id='map'>
+        <div id="loading"></div>
         <h4>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h4>
         <button id="btnMap" type="button" class="btn btn-light">Lets get started</button>
     </div>
