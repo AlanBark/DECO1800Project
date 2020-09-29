0ba53c989daa7d708d94e19c394bb1945bc280cc
AlanBark <a.parkes@uqconnect.edu.au>
2020-09-29 06:12:48 +0000

    deleted unused files. Added map-style.css

--- map.php
+++ map.php
@@ -11,11 +11,18 @@
 
 <div class="container" id="main-content">
 	<h2>Interactive Map</h2>
-    <?php
-    echo "<pre>";
-    $test = getAllSuburbs();
-    print_r($test);
-    ?>
+    <div id="mapid"></div>
+    <script>
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiYWxhbmJhcmsiLCJhIjoiY2tmbW5wcHU2MmQ2eDJxbG1mdmVlY3FleiJ9.N9kjUP7cI-ng_Yd7gSU0vA'
+        }).addTo(mymap);
+    </script>
 </div>
 <?php include("includes/footer.php");?>
 
