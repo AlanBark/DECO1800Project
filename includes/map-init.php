3872001047c7770943a8dd4e83256c1647bf3117
AlanBark <a.parkes@uqconnect.edu.au>
2020-10-21 06:23:55 +0000

    api-interaction made redundant, rewrote in js. Map page now requests raw geojson file from server, then requests api data from data.gov, with intent to combine using expressions in create_map(). Local storage now used over temp file storage.

--- includes/map-init.php
+++ includes/map-init.php
@@ -1,35 +1,5 @@
 <?php
-include("api-interaction.php");
-
-function initialiseMap($data) {
-
-    // associative array of suburbs and marriage frequency
-    $suburbs = getAllSuburbs();
-
-    $allSuburbs = json_decode(file_get_contents('../js/raw-data.json'), true);
-    
-    $count = 0;
-    foreach($allSuburbs['features'] as $i) {
-        $found = false;
-        foreach($suburbs as $suburb => $frequency) {
-            if ($i['properties']['qld_loca_2'] == $suburb) {
-                $found = true;
-                $allSuburbs['features'][$count]['properties']['qld_loca_3'] = $frequency;
-                break;
-            }
-        }
-        if (!$found) {
-            unset($allSuburbs['features'][$count]);
-        }
-        $count++;
-    }
-    $newJson = json_encode($allSuburbs);
-    file_put_contents('../tmp/filtered.geojson', $newJson);
-
-    echo "createMap();";
-}
-
-if (isset($_POST['callinitialiseMap'])) {
-    echo initialiseMap($_POST['callinitialiseMap']);
-}
+$payload = file_get_contents('../js/raw-data.json');
+header('Content-Type: application/json');
+echo $payload;
 ?>
