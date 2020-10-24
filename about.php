35a3229fb0b0ab6dd8416c2c346889f5e6df7c3f
whyhlf <jacky>
2020-10-24 00:51:28 +0000

    about us page upload

--- about.php
+++ about.php
@@ -10,10 +10,15 @@
 
 <div class="container about-wrapper" id="main-content">
 	<h2>About Us</h2>
-	<p>Some content goes here for about</p>
+	<h3 class="about-item">Yee's Team of Divorced Moms</h3></h3>
+    <p>This website is brought to you by Yee's Team of Divorced Moms, which consists of Yee, Alec, Jared and Leo.</p>
 
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+    <h3 class="about-item">Data Source</h3>
+    <p>The information displayed in the interactive map is retrieved from the Australian Government’s Open Data Portal (data.gov.au). These data sources include marriage statistics by month, suburb and gender. We use these sets of data to determine whether the time provided by users is peak wedding season, which may lead to higher prices. The data was originally presented as a table, however, we will analyse and display it through a graphical and visual representation in the form of an interactive map.</p>
+
+    <h3 class="about-item">Data Source Links</h3>
+    <p>Queensland Government (2020). Marriages by suburb. Queensland Government. <a href="https://www.data.qld.gov.au/dataset/marriages-by-suburb" style="color: blue;">https://www.data.qld.gov.au/dataset/marriages-by-suburb</a></p>
+    <p>Queensland Government (2020). Marriages by sex. Queensland Government. <a href="https://www.data.qld.gov.au/dataset/marriages-by-sex" style="color: blue;">https://www.data.qld.gov.au/dataset/marriages-by-sex</a></p>
 </div>
 
 <?php include("includes/footer.php");?>
